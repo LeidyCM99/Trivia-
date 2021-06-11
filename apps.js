9315b202d4fab3f01c2b46befd5fa439b1e00ee1
Leidy <paoalm1@hotmail.com>
2021-06-11 18:06:37 -0500

    first commit

---
+++ apps.js
@@ -0,0 +1,222 @@
+// Función para saludar con el nombre del usuario
+
+const nombre = '';
+
+window.onload = function saludar() {
+    const nombre = document.getElementById('casilla');
+    const contenedor = document.getElementById('contenedor');
+    contenedor.textContent = '¡Hola '+ nombre.value +' ! Escoje tu categoría';
+}
+
+//////////////////////////////////////////
+// Función para mostrar el cuestionario sobre Gastronomía
+
+function mostrarQuestionario1() {
+    
+    const questionario1 = document.getElementById('tercera').style.display;
+    console.log(questionario1);
+        if (questionario1 == 'none' || questionario1 == '') {
+            document.getElementById('tercera').style.display = 'block'
+        }else{
+            document.getElementById('tercera').style.display = 'none'
+        }
+
+    const categoria = document.getElementById('categoria').style.display;
+        if (categoria == 'block') {
+            document.getElementById('categoria').style.display = 'none'
+        }else{
+            document.getElementById('categoria').style.display = 'block'
+        }
+}
+
+
+//////////////////////////////////////////
+// Función para mostrar el cuestionario sobre Turismo
+
+function mostrarQuestionario2() {
+    console.log("---------------")
+    const questionario2 = document.getElementById('cuarta').style.display;
+        if (questionario2 == 'none' || questionario2 == '') {
+            document.getElementById('cuarta').style.display = 'block'
+        }else{
+            document.getElementById('cuarta').style.display = 'none'
+        }
+
+    const categoria = document.getElementById('categoria').style.display;
+        if (categoria == 'block') {
+            document.getElementById('categoria').style.display = 'none'
+        }else{
+            document.getElementById('categoria').style.display = 'block'
+        }
+}
+
+
+//////////////////////////////////////////
+//Función que captura las respuestas y suma los aciertos de Gastronomía
+
+function resultado() {
+
+    var p1, p2, p3, p4, p5, p6, p7, p8, nota;
+    
+    // PREGUNTA 1 
+    
+    if (document.getElementById('p12').checked==true) {p1=1}
+    
+    if (p1==1) {document.querySelectorAll('.pregunta1')[0]}
+    
+    else {p1=0}
+    
+    // PREGUNTA 2 
+    
+    if (document.getElementById('p21').checked==true) {p2=1}
+    
+    if (p2==1) {document.querySelectorAll('.pregunta2')[1]}
+    
+    else {p2=0}
+    
+    // PREGUNTA 3 
+    
+    if (document.getElementById('p33').checked==true) {p3=1}
+    
+    if (p3==1) {document.querySelectorAll('.pregunta3')[2]}
+    
+    else {p3=0}
+    
+    // PREGUNTA 4
+    
+    if (document.getElementById('p42').checked==true) {p4=1}
+    
+    if (p4==1) {document.querySelectorAll('.pregunta4')[3]}
+    
+    else {p4=0}
+     
+    // PREGUNTA 5
+    
+    if (document.getElementById('p51').checked==true) {p5=1}
+    
+    if (p5==1) {document.querySelectorAll('.pregunta5')[3]}
+    
+    else {p5=0}
+     
+    // PREGUNTA 6
+    
+    if (document.getElementById('p63').checked==true) {p6=1}
+    
+    if (p6==1) {document.querySelectorAll('.pregunta6')[3]}
+    
+    else {p6=0}
+     
+    // PREGUNTA 7
+    
+    if (document.getElementById('p72').checked==true) {p7=1}
+    
+    if (p7==1) {document.querySelectorAll('.pregunta7')[3]}
+    
+    else {p7=0}
+    
+    // PREGUNTA 8
+    
+    if (document.getElementById('p83').checked==true) {p8=1}
+    
+    if (p8==1) {document.querySelectorAll('.pregunta8')[3]}
+    
+    else {p8=0}
+
+    // Sumatoria
+    nota=p1+p2+p3+p4+p5+p6+p7+p8;
+
+    // console.log(p1 + '' + p2 + '' + p3 + '' + p4 + ''+ p5 + '' + p6 + ''+ p7 + '' +p8);
+    
+    document.getElementById('resultado').innerHTML="Aciertos: "+nota;
+    
+    rs=confirm("continuar");
+    
+    if (rs==false) {document.forms[0].reset();location.reload()}
+    
+    }
+
+
+/////////////////////////////////////////////////////
+//Función que captura las respuestas y suma los aciertos de Lugares Turísticos
+
+function resultado2() {
+    var q1, q2, q3, q4, q5, q6, q7, q8, nota;
+    
+    //  PREGUNTA 1
+    
+    if (document.getElementById('q13').checked==true) {q1=1}
+    
+    if (q1==1) {document.querySelectorAll('.pregunta1')[0]}
+    
+    else {q1=0}
+
+    
+    //  PREGUNTA 2
+    
+    if (document.getElementById('q22').checked==true) {q2=1}
+    
+    if (q2==1) {document.querySelectorAll('.pregunta2')[1]}
+    
+    else {q2=0}
+    
+    
+    //  PREGUNTA 3
+    
+    if (document.getElementById('p33').checked==true) {q3=1}
+    
+    if (q3==1) {document.querySelectorAll('.pregunta3')[2]}
+    
+    else {q3=0}
+    
+    //  PREGUNTA 4
+    
+    if (document.getElementById('q42').checked==true) {q4=1}
+    
+    if (q4==1) {document.querySelectorAll('.pregunta4')[3]}
+    
+    else {q4=0}
+    
+    //  PREGUNTA 5
+    
+    if (document.getElementById('q51').checked==true) {q5=1}
+    
+    if (q5==1) {document.querySelectorAll('.pregunta5')[3]}
+    
+    else {q5=0}
+    
+    //  PREGUNTA 6
+    if (document.getElementById('q63').checked==true) {q6=1}
+    
+    if (q6==1) {document.querySelectorAll('.pregunta6')[3]}
+    
+    else {q6=0}
+    
+    //  PREGUNTA 7
+    if (document.getElementById('q73').checked==true) {q7=1}
+        
+        if (q7==1) {document.querySelectorAll('.pregunta7')[3]}
+        
+        else {q7=0}
+        
+        //  PREGUNTA 8
+        
+        if (document.getElementById('q81').checked==true) {q8=1}
+        
+        if (q8==1) {document.querySelectorAll('.pregunta8')[3]}
+        
+        else {q8=0}
+        
+    
+    // Sumatoria
+    
+    nota=q1+q2+q3+q4+q5+q6+q7+q8;
+    console.log(q1+''+q2+''+q3+''+q4+''+q5+''+q6+''+q7+''+q8);
+    
+    document.getElementById('resultado2').innerHTML=" Aciertos: "+nota;
+    
+    rs=confirm("continuar");
+    
+    if (rs==false) {document.forms[0].reset();location.reload()}
+    
+    } 
+
---
+++ readme.md
@@ -0,0 +1,27 @@
+## Proceso de diseño:
+El proceso de diseño fue, en un principio, un poco abrumador hasta que leimos el documento guía, socializamos y reflexionamos acerca de los criterios de aceptación. Luego decidimos como hacer la división del trabajo. Comenzamos con el prototipado de baja fidelidad para tener una idea de cómo queríamos ver nuestro producto. Reflexionamos acerca de las pantallas, cuánto texto, cuáles imágenes y colores que pudieran transmitir al usuario la idea de nuestra trivia. Queríamos algo llamativo pero no recargado, breve, simple y fácil de comprender. 
+
+## ¿Por qué crees que el producto resuelve el problema del usuario?
+Nuestro usuario es aquel connacional que guste poner a prueba sus conocimentos. Además, como hay cerca de 4.5 millones de colombianos que viven alrededor del mundo, pensamos que una trivia que estimule la memoria sobre gastronomía y sitios turísticos del pais sería algo curioso, divertido y relajante.
+
+## Prototipos
+
+1- La primera pantalla es la de bienvenida. Se pide al usuario su nombre y se le ofrece un botón para comenzar a jugar.
+También hay un botón para saber las reglas del juego.
+ 
+<img src="img/pantalla_1.jpeg" alt="prototipo1" width='300'>
+
+2- La segunda foto muestra las dos categorías a escoger. Cada categoría tendrá una lista de 8 preguntas, cada una con 3 opciones para escojer la única respuesta correcta.
+
+<img src="img/pantalla_2.jpeg" alt="prototipo2" width='300'>
+
+3- La tercera pantalla  lleva a las opciones de cada pregunta.
+
+<img src="img/pantalla_3.jpeg" alt="prototipo3" width='300'>
+
+4- La última pantalla lleva al resultado de la trivia y ofrece un botón para regresar al inicio y volver a jugar si el ususario así lo desea.
+
+<img src="img/pantalla_4.jpeg" alt="prototipo4" width='300'>
+
+
+
